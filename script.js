8ec6bd073efb9ecd383c13f794b1dab51a4fde6a
piyush-agrawal6 <agrawaljoy1@gmail.com>
2023-01-01 11:00:50 +0530

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
-
+const hamburger = document.querySelector(".hamburger");
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
 let loadInterval;
+
+hamburger.onclick = function () {
+  let navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("active");
+};
 
 function loader(elem) {
   elem.textContent = "";
@@ -51,46 +56,49 @@ function chat(isAi, value, id) {
 const handleSubmit = async (e) => {
   e.preventDefault();
   const data = new FormData(form);
-  
-  if(!data.get("prompt").trim()){
-    alert("Please write something")
-  }else{
-      //user chat
-  chatContainer.innerHTML += chat(false, data.get("prompt").trim(), Date.now());
-  form.reset();
 
-  //bot chat
-  const newId = generateId();
-  chatContainer.innerHTML += chat(true, " ", newId);
-  // to focus scroll to the bottom
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-
-  const messageDiv = document.getElementById(newId);
-  loader(messageDiv);
-
-  // fetch data
-  const response = await fetch("https://vast-plum-codfish-gear.cyclic.app", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      prompt: data.get("prompt"),
-    }),
-  });
-  clearInterval(loadInterval);
-  messageDiv.innerHTML = "";
-  if (response.ok) {
-    const data = await response.json();
-    const parsedData = data.bot.trim();
-    typeText(messageDiv, parsedData);
+  if (!data.get("prompt").trim()) {
+    alert("Please write something");
   } else {
-    const err = await response.text();
-    messageDiv.innerHTML = "Something went wrong";
-    alert(err);
-  }
-  }
+    //user chat
+    chatContainer.innerHTML += chat(
+      false,
+      data.get("prompt").trim(),
+      Date.now()
+    );
+    form.reset();
 
+    //bot chat
+    const newId = generateId();
+    chatContainer.innerHTML += chat(true, " ", newId);
+    // to focus scroll to the bottom
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    const messageDiv = document.getElementById(newId);
+    loader(messageDiv);
+
+    // fetch data
+    const response = await fetch("https://vast-plum-codfish-gear.cyclic.app", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: data.get("prompt"),
+      }),
+    });
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = "";
+    if (response.ok) {
+      const data = await response.json();
+      const parsedData = data.bot.trim();
+      typeText(messageDiv, parsedData);
+    } else {
+      const err = await response.text();
+      messageDiv.innerHTML = "Something went wrong";
+      alert(err);
+    }
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
